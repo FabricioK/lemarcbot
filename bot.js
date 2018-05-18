511ec48c9dde1a2930a5b70d106bd10e20c7ff2a
Fabricio dos santos antunes <fabriciodossa@hotmail.com>
2018-05-18 10:12:36 -0300

    alterações

--- bot.js
+++ bot.js
@@ -1,29 +1,40 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+var firebase = require("firebase");
 
 var config = {
-    apiKey: "AIzaSyCvjz6ZeHdrYgUB4w__0VbjpWBrufUMxFg",
-    authDomain: "lemarc-bot.firebaseapp.com",
-    databaseURL: "https://lemarc-bot.firebaseio.com",
-    projectId: "lemarc-bot",
-    storageBucket: "lemarc-bot.appspot.com",
-    messagingSenderId: "430726932366"
-  };
-  firebase.initializeApp(config);
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAG_EBUCKET,
+  messagingSenderId: process.env.MESSAGIN_SENDER_ID
+};
+//firebase.initializeApp(config);
 
 client.on("ready", () => {
-  console.log("O GM está pronto!");
+  console.log("O lemarc está pronto!");
 });
 client.on("message", (message) => {
-    if (message.content.startsWith("!GM!")) {
-        switch (message.content) {
-          case "!gm! vou sair em uma aventura":
-              message.channel.send("ai que brava ela.");
-            break;
-            message.channel.send("Me desculpe, não entendi");
-          default:
-            break;
-        }        
-      }
+  if (message.content.startsWith("!lemarc")) {
+    switch (message.content.toLowerCase()) {
+      case message.content.startsWith("lemarc help!"):
+        message.channel.send("as opções são 'inicie seu pedido com 'quero' ou peça pelo 'cardápio'");
+        break;
+      case message.content.startsWith("!lemarc quero"):
+        message.channel.send("foda-se");
+        break;
+      case message.content.startsWith("!lemarc cardápio"):
+        message.channel.send("tem altas coisas");
+        break;
+        message.channel.send("Me desculpe, não entendi");
+      default:
+        break;
+    }
+  }
 });
-//client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
+
+
+
+
